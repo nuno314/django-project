5b8b2a8b613ca5843f9387b7c455338283c860f4
nuno314 <tinle1234vt@gmail.com>
2022-04-10 10:59:51 +0700

    done part 4: Static Files & Images

--- midterm/midterm/settings.py
+++ midterm/midterm/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]

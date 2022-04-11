f4a2d794622dad9182986f634bfae11cf54cdc3b
nuno314 <tinle1234vt@gmail.com>
2022-04-11 22:23:35 +0700

    done part 5: Database Models & Admin Panel

--- midterm/accounts/models.py
+++ midterm/accounts/models.py
@@ -1,3 +1,37 @@
+import email
+from sre_constants import CATEGORY
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door','Out Door'),
+    )
+    name = models.CharField(max_length=200)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, choices=CATEGORY)
+    description = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered','Delivered'),
+    )
+    # customer = 
+    # product = 
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
